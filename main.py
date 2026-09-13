a06a8148dd17ba6f06bbd552bbcc20127ba51936
jay1407404 <jasurbek4787@gmail.com>
2026-09-13 20:36:58 +0900

    Revise main.py to showcase Python built-ins
    Updated print statements and added built-in types information.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Python")
+# Dunder __builtins__, __init__
+message = "PHYTHON: Everything is object!"
+print(message)
+
+result = type(message)
+print("result:" result)
+
+''' in Python, there are builtin tools:
+(1) TYPES > int float str list dic
+(2) FUNCTIONS > print() len() input() type() str() int()
+(3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))
